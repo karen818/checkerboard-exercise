16c7116f0dcf1804038750d7d5b375a311808f8c
karen818 <klc0818@outlook.com>
2016-05-14 21:18:05 -0500

    adds flashing js; can't get setInterval to work

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 
-for (var i = 0; i < 63; i++) {
-    var tile = document.createElement("div");
-    tile.style.width = '11.1%';
-    tile.style.paddingBottom = '11.1%';
-    tile.style.cssFloat = 'left';
 
-    if (i % 2 === 0) {
-        tile.style.backgroundColor = 'red';
+window.onload = setInterval(function(){
+    for (var i = 0; i < 63; i++) {
+
+        var color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        var tile = document.createElement("div");
+
+        tile.style.width = '11.1%';
+        tile.style.paddingBottom = '11.1%';
+        tile.style.cssFloat = 'left';
+        tile.style.backgroundColor = color;
+
+        document.body.appendChild(tile);
     }
-    else {
-        tile.style.backgroundColor = 'black';
-    }
-    document.body.appendChild(tile);
-}
+}, 2000);
