38f0b52dff2d14fe107d6340966b9785c221ba74
karen818 <klc0818@outlook.com>
2016-05-14 20:11:40 -0500

    finishes black and red checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Your JS goes here
+
+for (var i = 0; i < 63; i++) {
+    var tile = document.createElement("div");
+    tile.style.width = '11.1%';
+    tile.style.paddingBottom = '11.1%';
+    tile.style.cssFloat = 'left';
+
+    if (i % 2 === 0) {
+        tile.style.backgroundColor = 'red';
+    }
+    else {
+        tile.style.backgroundColor = 'black';
+    }
+    document.body.appendChild(tile);
+}
